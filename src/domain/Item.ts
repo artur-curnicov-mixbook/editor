5ec9c0ef6a7f70b71f3ef0b813b2edaef1a6575b
Artur Curnicov <acurnicov@mixbook.com>
2023-06-27 09:45:44 +0300

    Add inner drag and drop

--- src/domain/Item.ts
+++ src/domain/Item.ts
@@ -1,6 +1,7 @@
 import { ItemType } from './ItemType';
+import { Moveable } from './Moveable';
 import { Positionable } from './Positionable';
 
-export interface Item extends Positionable {
+export interface Item extends Positionable, Moveable {
   type: ItemType;
 }
